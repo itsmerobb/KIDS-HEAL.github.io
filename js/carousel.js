4dc66aab4a43f18972e2454d1410254b53926d17
Austin Haas <austin@pettomato.com>
2016-04-01 17:51:47 -0700

    Show media inline, rather than in a modal popup.

--- js/carousel.js
+++ js/carousel.js
@@ -1,37 +1,104 @@
+function loadVideoContent (carousel, drawer, title, url, width, height){
+  $(drawer).find('.drawer-content').html(
+      '<div class="embed-responsive embed-responsive-16by9"> \
+<iframe class="media-embed embed-responsive-item" \
+src="" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> \
+</div>');
+  $(drawer).find(".media-embed").attr("src", url);
+  if($(drawer).is(":visible")){
+  }
+  else{
+    $(drawer).show();
+  }
+  $('html,body').animate({scrollTop: $(carousel).offset().top}, "slow");
+}
+
+function loadImageContent (carousel, drawer, title, url, width, height){
+  $(drawer).find('.drawer-content').html("<img class='img-responsive' src='' />")
+  $(drawer).find(".drawer-content img").attr("src", url);
+  $(drawer).show();
+  $('html,body').animate({scrollTop: $(carousel).offset().top}, "slow");
+}
+
+function closeContent (target){
+  $(target).hide();
+  $(target).find('.drawer-content').html("");
+}
+
 $(document).ready(function(){
-  $('.slick-carousel').slick();
+  $('.slick-carousel').slick(
+    {
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1300,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4
+          }
+        },
+        {
+          breakpoint: 1000,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        },
+        {
+          breakpoint: 700,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    }
+  );
 
-  $('.modal').on('show.bs.modal', function (event) {
+  $(".carousel").each(
+    function (event) {
+      var carousel = $(this);
+      var buttons  = carousel.find(".slick-carousel .thumb a");
+      var drawer   = carousel.find(".drawer");
+      var close    = drawer.find(".close-button");
 
-    // Button that triggered the modal
-    var button = $(event.relatedTarget)
+      buttons.each(function (){
 
-    // Extract info from data-* attributes
-    var title = button.data('title');
-    var url = button.data('url');
-    var width = button.data('width');
-    var height = button.data('height');
+        var button = $(this);
+        var title  = button.data('title');
+        var url    = button.data('url');
+        var width  = button.data('width');
+        var height = button.data('height');
 
-    var modal = $(this);
-    modal.find('.modal-title').text(title)
-    modal.find('.modal-body .media-embed').attr("src",url)
-    //modal.find('.modal-body .video-embed').attr("width",width)
-    //modal.find('.modal-body .video-embed').attr("height",height)
-  });
+        switch (button.data('type')) {
+        case "video":
+          button.click(function(){
+            loadVideoContent(carousel, drawer, title, url, width, height);
+            return false;
+          });
+          break;
+        case "image":
+          button.click(function(){
+            loadImageContent(carousel, drawer, title, url, width, height);
+            return false;
+          });
+          break;
+        }
+      });
 
-  $('.modal').on('shown.bs.modal', function () {
-      var modal = $(this);
-      var $iframe = modal.find('.modal-body .media-embed');
-      var contentWindow = $iframe[0].contentWindow;
-      var targetOriginUrl = $iframe.attr('src').split('?')[0];
-      contentWindow.postMessage({ 'method': 'play' }, targetOriginUrl);
-  });
-
-  $('.modal').on('hidden.bs.modal', function () {
-      var modal = $(this);
-      var $iframe = modal.find('.modal-body .media-embed');
-      var contentWindow = $iframe[0].contentWindow;
-      var targetOriginUrl = $iframe.attr('src').split('?')[0];
-      contentWindow.postMessage({ 'method': 'pause' }, targetOriginUrl);
-  });
+      close.click(function(){
+        closeContent(drawer);
+        return false;
+      });
+    }
+  );
 });
